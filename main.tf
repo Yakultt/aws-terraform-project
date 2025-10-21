576ddc7bff2317baf89718c742c021f12d9e7b44
Harit Sookmeewiriya <harit@Harits-MacBook-Pro.local>
2025-10-20 20:54:06 -0400

    new changes to the main file

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-//basically telling terraform what cloud service we are going to Use
+//basically telling terraform what cloud service we are going to Use 
 //cause terraform is kinda dumb
 terraform {
   required_version = ">= 1.13.0"
@@ -9,15 +9,21 @@ terraform {
     }
   }
 }
-//tells terraform configuration info about the aws provider
-//tells it when creating anything from the aws provider, you put it in the
+
+//tells terraform configuration info about the aws provider 
+//tells it when creating anything from the aws provider, you put it in the 
 //region us-east-1
+
 provider "aws" {
   region = "us-east-1"
 }
+//fetches the latest Amazon Linux 2023 x86_64 AMI
+data "aws_ssm_parameter" "al2023_x86" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
 
-//creating the acutal resource
-resource "aws_instance" "first_ec2_instance" {
+// creates a tiny free-tier EC2 instance
+resource "aws_instance" "vm" {
   ami           = data.aws_ssm_parameter.al2023_x86.value
   instance_type = "t3.micro"
 
