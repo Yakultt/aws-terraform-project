4c2d8c0af975a1deb495a2d271446c83ba30d6ff
Harit Sookmeewiriya <harit@Harits-MacBook-Pro.local>
2025-11-10 14:02:21 -0500

    new commits

--- main.tf
+++ main.tf
@@ -104,6 +104,13 @@ resource "aws_security_group" "allow_ssh_http_https" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 8443
+    to_port = 8443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
